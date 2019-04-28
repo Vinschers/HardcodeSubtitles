7ab38de610070f114dfd8ffda0de905195f60a61
Scherer <fsvicentin@hotmail.com>
2019-04-28 01:26:46 -0300

    aaa

--- HardcodeSubtitles/Program.cs
+++ HardcodeSubtitles/Program.cs
@@ -27,7 +27,8 @@ namespace HardcodeSubtitles
                 cmd.StartInfo.UseShellExecute = false;
                 cmd.Start();
 
-                cmd.StandardInput.WriteLine("cd C:\\");
+                cmd.StandardInput.WriteLine(fbd.SelectedPath.Substring(0, 1) + ":");
+                cmd.StandardInput.WriteLine("cd " + fbd.SelectedPath.Substring(0, 1) + ":\\");
                 cmd.StandardInput.WriteLine("cd " + fbd.SelectedPath);
                 cmd.StandardInput.Flush();
                 cmd.StandardInput.Close();
@@ -36,55 +37,22 @@ namespace HardcodeSubtitles
                 string comando = "";
                 foreach (var path in Directory.GetFiles(fbd.SelectedPath))
                 {
-                    comando = "HandBrakeCLI -i \"" + Path.GetFileName(path) + "\" -o \"" +
+                    comando = "handbrakecli -i \"" + Path.GetFileName(path) + "\" -o \"" +
                         Path.GetFileName(path).Substring(0, Path.GetFileName(path).LastIndexOf(".")) +
                         ".mp4\" -e x264 -N \"por\" --subtitle-burn";
-                    Console.WriteLine(Path.GetFileName(path)); // file name
                     new Thread(() => Cmd(comando, fbd.SelectedPath)).Start();
+                    Thread.Sleep(500);
                 }
                 Console.WriteLine(cmd.StandardOutput.ReadToEnd());
                 cmd.WaitForExit();
                 cmd.Close();
+                //Console.Read();
             }
         }
         static void Cmd(string comando, string path)
         {
-            //Process cmd = new Process();
-            //cmd.StartInfo.FileName = "cmd.exe";
-            //cmd.StartInfo.RedirectStandardInput = true;
-            //cmd.StartInfo.RedirectStandardOutput = true;
-            //cmd.StartInfo.CreateNoWindow = true;
-            //cmd.StartInfo.UseShellExecute = false;
-            //cmd.Start();
-
-            //cmd.StandardInput.WriteLine("cd C:\\");
-            //cmd.StandardInput.WriteLine("cd " + path);
-            //cmd.StandardInput.WriteLine(comando);
-            //cmd.StandardInput.Flush();
-            //cmd.StandardInput.Close();
-            //cmd.WaitForExit();
-            //Console.WriteLine(cmd.StandardOutput.ReadToEnd());
-
-            Process.Start("CMD.exe", "/K cd C:\\&cd " + path + "&" + comando);
-
-            //Process p = new Process();
-            //ProcessStartInfo info = new ProcessStartInfo();
-            //info.FileName = "cmd.exe";
-            //info.RedirectStandardInput = true;
-            //info.UseShellExecute = false;
-
-            //p.StartInfo = info;
-            //p.Start();
-
-            //using (StreamWriter sw = p.StandardInput)
-            //{
-            //    if (sw.BaseStream.CanWrite)
-            //    {
-            //        sw.WriteLine("cd C:\\");
-            //        sw.WriteLine("echo oiiiiiiiii");
-            //        sw.WriteLine("echo \"pf funciona\"");
-            //    }
-            //}
+            Console.Write(comando);
+            Process.Start("CMD.exe", "/K " + path.Substring(0,1) + ":&cd " + path + "&" + comando);
         }
     }
 }
